fdaec59f02edf4dfdf4d28875dcef87f7d535c8e
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2023-06-27 19:35:07 +0700

    include footer

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -215,49 +215,7 @@
    </a>
 </div>
 <!-- end floating button -->
-    <footer class="m-0 p-2 px-5 text-white" style="background: rgb(153,153,153);">
-
-        <div class="d-flex flex-column align-items-center fw-light ">
-            <img src="./img/logore.png " width="80px" alt="">
-            <div class="px-4 text-center">
-                <h4>องค์การบริหารส่วนตำบลสะนอ</h4>
-                <div>หมู่ 1 ตำบลสะนอ อำเภอยะรัง จังหวัดปัตตานี รหัสไปรษณีย์ 94160</div>
-                <div>โทรศัพท์/โทรสาร : 0-7343-927</div>
-                <div>E-mail : office@sanor.go.th</div>
-
-            </div>
-
-            <div class="py-3 ">
-                <a target="_blank"
-                    href="https://www.facebook.com/people/%E0%B8%AD%E0%B8%9A%E0%B8%95%E0%B8%AA%E0%B8%B0%E0%B8%99%E0%B8%AD-%E0%B8%AD%E0%B8%A2%E0%B8%B0%E0%B8%A3%E0%B8%B1%E0%B8%87-%E0%B8%88%E0%B8%9B%E0%B8%B1%E0%B8%95%E0%B8%95%E0%B8%B2%E0%B8%99%E0%B8%B5/100010671091367/?locale=th_TH">
-                    <h3 class="text-center bi bi-facebook"></h3>
-                </a>
-                <div class="text-center ">
-                    <ul class="d-flex align-items-center  list-unstyled">
-                        <a href="">
-                            <li class="p-2 ">หน้าหลัก</li>
-                        </a>
-                        |
-                        <a href="">
-                            <li class="p-2 ">ติดต่อเรา</li>
-                        </a>
-                        |
-                        <a href="">
-                            <li class="p-2 ">แผนผังเว็บไซต์</li>
-                        </a>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-        <div>
-
-            <p class="fw-light">
-                © สงวนลิขสิทธิ์ 2565 องค์การบริหารส่วนตำบลสะนอ จังหวัดปัตตานี <a href="#">Privacy</a> ·
-                <a href="#">Terms</a>
-            </p>
-        </div>
-    </footer>
+@include('footer')
 
     <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
